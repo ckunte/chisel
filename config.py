2ce428f24161b2bb57c38cae364b29a4716749cf
Chetan Kunte <ck@eml.cc>
2023-04-07 09:53:26 +0800

    Update FEED time format

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 # Chisel configuration
-POSTS = "Sites/notes/"            # location of posts (in markdown) folder 
-WWW = "Sites/ck.lo/"              # local www folder (upload contents to a web host)
-TMPL = "Sites/chisel/plain/" 	  # jinja templates folder
+POSTS = "Sites/notes/"  # location of posts (in markdown) folder
+WWW = "Sites/ck.lo/"  # local www folder (upload contents to a web host)
+TMPL = "Sites/chisel/plain/"  # jinja templates folder
 # Number of posts to show in RSS feed
 RSS_SHOW = 3
 # For URLs without ending in .html
 EXT = ["", ".html"]  # [URLEXT, PATHEXT]
 ## For URLs ending in .html
-#EXT = [".html", ""]  # [URLEXT, PATHEXT]
-TFMT = [                        # Various date and time formats
-    "%B %-d, %Y",               # Date as shown on html pages
-    "%a, %d %b %Y %H:%M:%S %z", # Date and time for RSS feed
-    "%Y-%m-%d %H:%M"            # Time format to use in markdown posts
-    ]
+# EXT = [".html", ""]  # [URLEXT, PATHEXT]
+TFMT = [  # Various date and time formats
+    "%B %-d, %Y",  # Date as shown on html pages
+    "%a, %d %b %Y %H:%M:%S %z",  # Date and time for RSS feed
+    "%Y-%m-%d %H:%M",  # Time format to use in markdown posts
+    "%Y-%m-%dT%H:%M:00+08:00",  # Time format for FEED
+]
