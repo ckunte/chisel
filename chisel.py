b93d4dfeb9964230498f0b895d5c74648a28e230
Chetan Kunte <ck@eml.cc>
2023-04-07 12:38:54 +0800

    mv unused templates to 'unused folder'

--- chisel.py
+++ chisel.py
@@ -86,15 +86,15 @@ def write_feed(url, data):
         f.write(data)
 
 
-def write_sitemap(url, data):
-    path = pathlib.Path(LOC[1]) / url
-    with gzip.open(path, "wb") as f:
-        f.write(data.encode("UTF-8"))
+# def write_sitemap(url, data):
+#    path = pathlib.Path(LOC[1]) / url
+#    with gzip.open(path, "wb") as f:
+#        f.write(data.encode("UTF-8"))
 
 
-@step
-def gen_sitemap(f, e):
-    write_sitemap("sitemap.xml.gz", e.get_template("sitemap.html").render(entries=f))
+# @step
+# def gen_sitemap(f, e):
+#    write_sitemap("sitemap.xml.gz", e.get_template("sitemap.html").render(entries=f))
 
 
 @step
