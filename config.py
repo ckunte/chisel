5a2ebff022863749b03f6cf8bce23958eed30807
Chetan Kunte <ck@eml.cc>
2023-04-15 20:37:03 +0800

    update chisel for a micro blog

--- config.py
+++ config.py
@@ -1,14 +1,7 @@
 # Chisel configuration
-POSTS = "Sites/wkly/"          # location of posts (in markdown) folder 
-WWW = "Sites/ck.lo/"         # local www folder (upload contents to a web host)
-TMPL = "Sites/chisel/plain/"   # jinja templates folder
-
+POSTS = "Sites/wkly/"  # location of posts (in markdown) folder
+WWW = "Sites/home.lo/"  # local www folder (upload contents to a web host)
+TMPL = "Sites/chisel/plain/"  # jinja templates folder
 SHOWPOSTS = [1, 12, 54]
-
-TFMT = [
-  "%Y-%m-%d %H:%M",
-  "%Y-%m-%dT%H:%M:00+08:00",
-  "%b %d %H:%M"
-]
-
-EXT = ["", ".html"] # URLEXT, PATHEXT
+TFMT = ["%Y-%m-%d %H:%M", "%Y-%m-%dT%H:%M:00+08:00"]
+EXT = ["", ".html"]  # URLEXT, PATHEXT
--- mc.py
+++ mc.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # mc.py -- 2021 C Kunte
-import os, re, time, datetime
-import markdown, jinja2
+import sys
+import time
+import datetime
+import os
+import pathlib
+import jinja2
+import markdown
 from functools import cmp_to_key
 from config import *
 
 
 LOC = [
-    os.environ["HOME"] + "/" + POSTS, 
-    os.environ["HOME"] + "/" + WWW,
-    os.environ["HOME"] + "/" + TMPL
+    os.path.join(os.environ["HOME"], POSTS),
+    os.path.join(os.environ["HOME"], WWW),
+    os.path.join(os.environ["HOME"], TMPL),
 ]
 
 
-FORMAT = lambda text: markdown.markdown(text,extensions=['smarty','footnotes'])
+FORMAT = lambda text: markdown.markdown(text, extensions=["smarty", "footnotes"])
 
 
 STEPS = []
@@ -22,9 +27,10 @@ STEPS = []
 
 def step(func):
     def wrapper(*args, **kwargs):
-        print("\t\tGenerating %s..." %func.__name__, end="");
+        print(f"\t\tGenerating {func.__name__}...", end="")
         func(*args, **kwargs)
         print("done.")
+
     STEPS.append(wrapper)
     return wrapper
 
@@ -33,73 +39,76 @@ def get_tree(source):
     files = []
     for root, ds, fs in os.walk(source):
         for name in fs:
-            if name[0] == ".": continue
-            if not re.match(r'^.+\.(md|mdown)$', name): continue
+            if name.startswith(".") or not name.endswith((".md", ".mdown")):
+                continue
             path = os.path.join(root, name)
-            f = open(path, "r")
-            title = f.readline().strip('\n\t')
-            date = time.strptime(f.readline().strip(), TFMT[0])
-            year, month, day, hour, minute = date[:5]
-            week = datetime.date(year, month, day).isocalendar()[1]
-            files.append({
-              'title': title,
-              'date': date,
-              'epoch': time.mktime(date),
-              'content': FORMAT(''.join(f.readlines()[1:])),
-              'feed_date': time.strftime(TFMT[1], date),
-              'year': year,
-              'week': week
-              })
-            f.close()
+            with open(path, "r") as f:
+                title = f.readline().strip("\n\t")
+                date_str = f.readline().strip()
+                date = time.strptime(date_str, TFMT[0])
+                year, month, day, hour, minute = date[:5]
+                week = datetime.date(year, month, day).isocalendar()[1]
+                files.append(
+                    {
+                        "title": title,
+                        "epoch": time.mktime(date),
+                        "content": FORMAT("".join(f.readlines()[1:])),
+                        "feed_date": time.strftime(TFMT[1], date),
+                        "year": year,
+                        "week": week,
+                    }
+                )
     return files
 
 
 def compare_entries(x, y):
-    result = (y['epoch'] > x['epoch']) - (y['epoch'] < x['epoch'])
+    result = (y["epoch"] > x["epoch"]) - (y["epoch"] < x["epoch"])
     if result == 0:
-        return (y['filename'] > x['filename']) - (y['filename'] < x['filename'])
+        return (y["filename"] > x["filename"]) - (y["filename"] < x["filename"])
     return result
 
 
 def write_file(url, data):
-    path = LOC[1] + url + EXT[1]
-    dirs = os.path.dirname(path)
-    if not os.path.isdir(dirs):
-        os.makedirs(dirs)
-    file = open(path, "w")
-    file.write(data)
-    file.close()
+    path = pathlib.Path(LOC[1]) / f"{url}{EXT[1]}"
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with open(path, "w") as f:
+        f.write(data)
 
-    
+
 def write_feed(url, data):
-    path = LOC[1] + url
-    file = open(path, "w")
-    file.write(data)
-    file.close()
+    path = pathlib.Path(LOC[1]) / url
+    with open(path, "w") as f:
+        f.write(data)
 
 
 @step
-def home(f, e):
-    write_file('index%s' %EXT[0], e.get_template('home.html').render(entries=f[:SHOWPOSTS[0]]))
+def homepage(f, e):
+    write_file(
+        f"index{EXT[0]}", e.get_template("home.html").render(entries=f[: SHOWPOSTS[0]])
+    )
 
 
 @step
 def feed(f, e):
-    write_feed('rss.xml', e.get_template('atom.xml').render(entries=f[:SHOWPOSTS[1]]))
+    write_feed(
+        "feed.json", e.get_template("feed.json").render(entries=f[: SHOWPOSTS[1]])
+    )
 
 
 @step
 def weeknotes(f, e):
-    write_file('w%s' %EXT[0], e.get_template('weeknotes.html').render(entries=f[:SHOWPOSTS[2]]))
+    write_file(
+        f"w{EXT[0]}", e.get_template("weeknotes.html").render(entries=f[: SHOWPOSTS[2]])
+    )
 
 
 def main():
-    print("Chiseling...");
-    print("\tReading files...", end="");
+    print("Chiseling...")
+    print("\tReading files...", end="")
     files = sorted(get_tree(LOC[0]), key=cmp_to_key(compare_entries))
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(LOC[2]))
     print("done.")
-    print("\tRunning steps...");
+    print("\tRunning steps...")
     for step in STEPS:
         step(files, env)
     print("\tdone.")
