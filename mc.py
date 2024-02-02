3b4b0af8aa2a5d2a78258215ce091f4821960f26
Chetan Kunte <ck@eml.cc>
2024-02-02 19:16:18 +0530

    add weeknotes page; update feed.json permalinks

--- mc.py
+++ mc.py
@@ -99,12 +99,12 @@ def feed(f, e):
     )
 
 
-# @step
-# def weeknotes(f, e):
-#    write_file(
-#        f"w{EXT[0]}",
-#        e.get_template("weeknotes.html").render(entries=f[: SHOWPOSTS[2]]),
-#    )
+@step
+def weeknotes(f, e):
+    write_file(
+        f"w{EXT[0]}",
+        e.get_template("weeknotes.html").render(entries=f[: SHOWPOSTS[2]]),
+    )
 
 
 def main():
